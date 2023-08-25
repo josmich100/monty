7840f0af236a73753952582fac934a5af5497f6c
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-08-24 18:12:30 -0700

    atoi

--- _atoi.c
+++ _atoi.c
@@ -1,5 +1,6 @@
 #include "monty.h"
 #include <limits.h>
+
 /**
  * _atoi - check the code
  *
@@ -9,29 +10,20 @@
  */
 int _atoi(char *s)
 {
-	unsigned int result = 0;
-	int sign = 1;
-	int i = 0;
+	int sign = 1, i = 0;
+	unsigned int res = 0;
 
-	while (s[i] != '\0')
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-		}
 		i++;
 	}
-
-	if (result > (unsigned long)INT_MAX && sign == 1)
-		return (0);
-	if (result > (unsigned long)INT_MAX + 1 && sign == -1)
-		return (0);
-
-	return ((int)result * sign);
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		res = (res * 10) + (s[i] - '0');
+		i++;
+	}
+	res *= sign;
+	return (res);
 }
